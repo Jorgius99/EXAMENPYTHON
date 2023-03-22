ed6da3cc19c1ec65447c5eaf754a931057529877
Jorgius99 <jmarzoz@epsg.upv.es>
2023-03-22 10:30:54 +0100

    commit_2

--- functions.py
+++ functions.py
@@ -1,2 +1,13 @@
-def read_data(nombreFichero):
-    f.open(nombreFichero, 'r')
+def read_data(nombre_fichero):
+    data = {}
+    with open(nombre_fichero, "r") as file:
+        header = file.readline().strip().split(";")
+        for column in header:
+            data[column] = []
+        for line in file:
+            values = line.strip().split(";")
+            for i in range(len(values)):
+                data[header[i]].append(values[i])
+    return data
+
+print(read_data("winequality.csv"))
