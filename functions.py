f4ad6a5e5e3a4444778a613bafea57fb63419f5e
Jorgius99 <jmarzoz@epsg.upv.es>
2023-03-22 10:46:20 +0100

    commit_3

--- functions.py
+++ functions.py
@@ -2,12 +2,11 @@ def read_data(nombre_fichero):
     data = {}
     with open(nombre_fichero, "r") as file:
         header = file.readline().strip().split(";")
-        for column in header:
-            data[column] = []
         for line in file:
             values = line.strip().split(";")
-            for i in range(len(values)):
-                data[header[i]].append(values[i])
+            #data[values[0]] = values[1:]
+            data={header[i]:values[i] for i in range(len(header)) : values[i] for i in range(len(values))}
     return data
+
 
 print(read_data("winequality.csv"))
